60ccd566460737cfc9597f7690c6cff69a789b66
zhangbin <359529353@qq.com>
2018-09-10 20:59:26 +0800

    添加单位转换工具

---
+++ BW_Libs/src/main/java/com/bloodcrown/baselibs/DisplayUtils.kt
@@ -0,0 +1,43 @@
+package com.bloodcrown.baselibs
+
+import android.content.Context
+
+/**
+ * 作者 ： BloodCrown
+ * 时间 ： 2018/9/10 下午8:51
+ * 描述 ：
+ */
+class DisplayUtils {
+
+    companion object {
+
+        /**
+         * px 转 dp
+         */
+        fun px2dp(context: Context, pxValue: Float): Int {
+            return (pxValue / context.getResources().getDisplayMetrics().density + 0.5f).toInt()
+        }
+
+        /**
+         * px 转 sp
+         */
+        fun px2sp(context: Context, pxValue: Float): Int {
+            return (pxValue / context.getResources().getDisplayMetrics().density + 0.5f).toInt()
+        }
+
+        /**
+         * dp 转 px
+         */
+        fun dp2px(context: Context, dipValue: Float): Int {
+            return (context.getResources().getDisplayMetrics().density * dipValue + 0.5f).toInt()
+        }
+
+        /**
+         * sp 转 px
+         */
+        fun sp2px(context: Context, spValue: Float): Int {
+            return (context.getResources().getDisplayMetrics().density * spValue + 0.5f).toInt()
+        }
+    }
+
+}
